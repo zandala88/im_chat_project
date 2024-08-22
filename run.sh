6a6ae36a4c81ea28dd8ec49a58cad992cac1b570
lkx <zandala_lkx@163.com>
2024-08-22 11:25:07 +0800

    fix: run.sh修改

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ current_date=$(date +'%Y-%m-%d-%H:%M:%S')
 sleep 1
 mkdir -p runlog
 echo "running code..."
-nohup go run main.go &
+go run main.go &
 echo "app is running, fetching port info..."
 
 sleep 2
